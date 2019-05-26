ab47cfb72795877bd95b37dab92f9e10d410b92c
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2019-05-26 16:09:20 -0300

    add setting varibles for admin lte

--- config_lte.php
+++ config_lte.php
@@ -1,5 +1,23 @@
 <?php
 //change to FALSE if you want back to appgini default
+$LTE_globals =[
+    app-title-prefix => "Ale | ", //window bar prfix title or browser tab
+    logo-mini => "glyphicon glyphicon-home", //mini logo for sidebar mini 50x50 pixels
+    logo-mini-text => "ALE", // text for side bar
+    navbar-text => "Alejandro Landini template",
+    footer-left-text => "<strong>ALE © ". date("Y") ." <a href=\"#\">Piattaforma Digitale Management System</a>.</strong>",
+    footer-right-text => "Anything you want"
+];
+
+ //changue this for tablename icon
+ $ico_menu = '{
+    "Orders":"fa fa-table",
+    "Gift":"fa fa-gift",
+    "Pencil":"fa fa-pencil-square-o",
+    "Cog":"fa fa-cog",
+    "Plus":"fa fa-plus",
+    "slash":"fa fa-eye-slash"
+}';
 
 function getLteStatus($LTE_enable = true){
     if(!function_exists('getMemberInfo')){
