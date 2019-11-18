21f35fa9754cae1506afffd37152bcc73378154f
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2019-11-18 15:04:35 -0300

    update file location

--- footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'config_lte.php';
-
-    if (getLteStatus()){
-        include_once("footer_lte.php");
-    }else{
-        include_once("footer_old.php");
-    }
--- header.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'config_lte.php';
-
-    if (getLteStatus()){
-        include_once("header_lte.php");
-    }else{
-        include_once("header_old.php");
-    }
